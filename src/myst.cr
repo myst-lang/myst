828145afaf30ade056d5045f4423c2691a16c76c
Jon Egeland <jonegeland@gmail.com>
2017-06-10 13:03:00 -0400

    Read source file name from command line. The Myst executable now expects an argument from the command line that names the file to be read as source.

--- src/myst.cr
+++ src/myst.cr
@@ -1,6 +1,13 @@
 require "./myst/vm"
 
+source_file = ARGV[0]?
+
+unless source_file
+  STDERR.puts("No source file given")
+  exit 1
+end
+
 vm = Myst::VM::VM.new
-vm.load("./spec/fibonnaci.mtc")
+vm.load(source_file)
 vm.dump(STDOUT)
 vm.run
