f3994267cfb8e8ce96802e0568885eccc8f39074
Jon Egeland <jonegeland@gmail.com>
2017-08-25 00:30:55 -0400

    [interpreter] Check type of existing value when redefining functions. If the existing value matching the name of the new function is _not_ a functor, raise an error about the redefinition as a new type.

--- src/myst/interpreter/visitors/function.cr
+++ src/myst/interpreter/visitors/function.cr
@@ -1,7 +1,10 @@
 class Myst::Interpreter
   def visit(node : AST::FunctionDefinition)
-    if (functor = @symbol_table[node.name]?) && functor.is_a?(TFunctor)
-      functor.add_clause(node)
+    if (functor = @symbol_table[node.name]?)
+      if functor.is_a?(TFunctor)
+        functor.add_clause(node)
+      else
+        raise "Redefinition of `#{node.name}` as a function. Already defined as a #{functor.type_name}"
     else
       functor = TFunctor.new(node, @symbol_table.current_scope)
       @symbol_table[node.name] = functor
