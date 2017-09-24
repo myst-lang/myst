3bc39fb139f4562fa7da99fdbaefb06fab564264
Jon Egeland <jonegeland@gmail.com>
2017-09-24 14:48:53 -0400

    [spec] Add interpreter specs for nested List and Map literals.

--- spec/interpreter/literals_spec.cr
+++ spec/interpreter/literals_spec.cr
@@ -17,7 +17,9 @@ describe "Interpreter - Literals" do
   it_interprets l([1]),         [TList.new([TInteger.new(1_i64)] of Myst::Value)]
   it_interprets l([1, 2, 3]),   [TList.new([TInteger.new(1_i64), TInteger.new(2_i64), TInteger.new(3_i64)] of Myst::Value)]
   it_interprets l([nil, :hi]),  [TList.new([TNil.new, TSymbol.new("hi")] of Myst::Value)]
+  it_interprets l([[1, 2], [3, 4]]), [TList.new([TList.new([TInteger.new(1_i64), TInteger.new(2_i64)] of Myst::Value), TList.new([TInteger.new(3_i64), TInteger.new(4_i64)] of Myst::Value)] of Myst::Value)]
 
   it_interprets l({ :a => 1 }),                       [TMap.new({ TSymbol.new("a") => TInteger.new(1_i64) } of Myst::Value => Myst::Value)]
   it_interprets l({ i(1) => "int", i(nil) => :nil }), [TMap.new({ TInteger.new(1_i64) => TString.new("int"), TNil.new => TSymbol.new("nil") } of Myst::Value => Myst::Value)]
+  it_interprets l({ {:a => 1 } => { :b => 2 } }),     [TMap.new({ TMap.new({ TSymbol.new("a") => TInteger.new(1_i64) } of Myst::Value => Myst::Value) => TMap.new({ TSymbol.new("b") => TInteger.new(2_i64) } of Myst::Value => Myst::Value) } of Myst::Value => Myst::Value)]
 end
