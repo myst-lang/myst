4f34f7493292acb1f14e3812123b233a82621348
Jon Egeland <jonegeland@gmail.com>
2017-09-24 16:33:16 -0400

    [spec] Add interpolation specs for And/Or.

--- spec/interpreter/interpolation_spec.cr
+++ spec/interpreter/interpolation_spec.cr
@@ -3,24 +3,28 @@ require "../support/nodes.cr"
 require "../support/interpret.cr"
 
 describe "Interpreter - Interpolation" do
-  # Interpolations of literals just push the literal value onto the stack.
-  it_interprets i(nil),     [TNil.new]
+  it_interprets i(nil), [val(nil)]
 
-  it_interprets i(true),    [TBoolean.new(true)]
-  it_interprets i(false),   [TBoolean.new(false)]
+  it_interprets i(true),   [val(true)]
+  it_interprets i(false),  [val(false)]
 
-  it_interprets i(1),       [TInteger.new(1_i64)]
-  it_interprets i(1.0),     [TFloat.new(1_f64)]
+  it_interprets i(1),    [val(1)]
+  it_interprets i(1.0),    [val(1.0)]
 
-  it_interprets i("hello"), [TString.new("hello")]
-  it_interprets i(:hello),  [TSymbol.new("hello")]
+  it_interprets i("hello"), [val("hello")]
+  it_interprets i(:hello),  [val(:hello)]
 
-  it_interprets i([1]),         [TList.new([TInteger.new(1_i64)] of Myst::Value)]
-  it_interprets i([1, 2, 3]),   [TList.new([TInteger.new(1_i64), TInteger.new(2_i64), TInteger.new(3_i64)] of Myst::Value)]
-  it_interprets i([nil, :hi]),  [TList.new([TNil.new, TSymbol.new("hi")] of Myst::Value)]
-  it_interprets i([[1, 2], [3, 4]]), [TList.new([TList.new([TInteger.new(1_i64), TInteger.new(2_i64)] of Myst::Value), TList.new([TInteger.new(3_i64), TInteger.new(4_i64)] of Myst::Value)] of Myst::Value)]
+  it_interprets i([1]),         [TList.new([val(1)])]
+  it_interprets i([1, 2, 3]),   [TList.new([val(1), val(2), val(3)])]
+  it_interprets i([nil, :hi]),  [TList.new([val(nil), val(:hi)])]
+  it_interprets i([[1, 2], [3, 4]]), [TList.new([TList.new([val(1), val(2)]), TList.new([val(3), val(4)])] of Myst::Value)]
 
-  it_interprets i({ :a => 1 }),                       [TMap.new({ TSymbol.new("a") => TInteger.new(1_i64) } of Myst::Value => Myst::Value)]
-  it_interprets i({ i(1) => "int", i(nil) => :nil }), [TMap.new({ TInteger.new(1_i64) => TString.new("int"), TNil.new => TSymbol.new("nil") } of Myst::Value => Myst::Value)]
-  it_interprets i({ {:a => 1 } => { :b => 2 } }),     [TMap.new({ TMap.new({ TSymbol.new("a") => TInteger.new(1_i64) } of Myst::Value => Myst::Value) => TMap.new({ TSymbol.new("b") => TInteger.new(2_i64) } of Myst::Value => Myst::Value) } of Myst::Value => Myst::Value)]
+  it_interprets i({ :a => 1 }),                       [TMap.new({ val(:a) => val(1) })]
+  it_interprets i({ i(1) => "int", i(nil) => :nil }), [TMap.new({ val(1) => val("int"), val(nil) => val(:nil) })]
+  it_interprets i({ {:a => 1 } => { :b => 2 } }),     [TMap.new({ TMap.new({ val(:a) => val(1) }) => TMap.new({ val(:b) => val(2) }) } of Myst::Value => Myst::Value)]
+
+  it_interprets %q(<(true   && true)>),   [val(true)]
+  it_interprets %q(<(false  && true)>),   [val(false)]
+  it_interprets %q(<(false  || true)>),   [val(true)]
+  it_interprets %q(<(true   || true)>),   [val(true)]
 end
