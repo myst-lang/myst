320cd063e772b8fdc128f0ac086c5629185c29d9
Jon Egeland <jonegeland@gmail.com>
2017-09-29 09:59:28 -0400

    [spec] Add specs for `obj.member` syntax for Calls. For now, this is probably all the coverage that the Call node will have. More specs will be added specifically around the native lib methods, as well as on the TFunctor and TNativeFunctor Value types.

--- spec/interpreter/nodes/call_spec.cr
+++ spec/interpreter/nodes/call_spec.cr
@@ -6,14 +6,12 @@ require "../../support/interpret.cr"
 # of the interpreter to evaluate them. Coverage of infix operators, native
 # functions, and standard library functions will be done separately.
 #
-# This section of tests relies on the Integer type having a `+` method
+# This section of tests relies on the Integer type with `+` and `to_s` methods
 # defined.
 describe "Interpreter - Call" do
-  it_interprets %q(1 + 1),      [val(2)]
-  it_interprets %q(1 + 1 + 1),  [val(3)]
+  it_interprets %q(1 + 1),        [val(2)]
+  it_interprets %q(1 + 1 + 1),    [val(3)]
 
-  it "does not add Integers and Strings" do
-    error = expect_raises{ parse_and_interpret %q(1 + "hello") }
-    (error.message || "").downcase.should match(/invalid argument/)
-  end
+  it_interprets %q(1.to_s),       [val("1")]
+  it_interprets %q((1 + 1).to_s), [val("2")]
 end
--- src/myst/interpreter/native_lib/integer.cr
+++ src/myst/interpreter/native_lib/integer.cr
@@ -1,7 +1,7 @@
 module Myst
-  TInteger::METHODS["+"] = TNativeFunctor.new do |this, args, _block_arg, itr|
+  TInteger::METHODS["+"] = TNativeFunctor.new do |this, (arg), _block, _itr|
     this = this.as(TInteger)
-    case arg = args[0]
+    case arg
     when TInteger
       TInteger.new(this.value + arg.value)
     when TFloat
@@ -9,5 +9,10 @@ module Myst
     else
       raise "invalid argument for Integer#+: #{arg.type_name}"
     end
+  end
+
+  TInteger::METHODS["to_s"] = TNativeFunctor.new do |this, _args, _block, _itr|
+    this = this.as(TInteger)
+    TString.new(this.value.to_s)
   end
 end
