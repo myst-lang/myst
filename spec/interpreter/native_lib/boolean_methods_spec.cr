ffceeec127b7b5abc804274fe9b52cac86908f55
Jon Egeland <jonegeland@gmail.com>
2017-10-02 21:29:28 -0400

    [nativelib] Add `Boolean#==` and `Boolean#!=`. The equality methods on Boolean are more strict than a truthiness check. For the result to be positive, the argument _must_ be a boolean value. Any other value will return a negative value.

--- spec/interpreter/native_lib/boolean_methods_spec.cr
+++ spec/interpreter/native_lib/boolean_methods_spec.cr
@@ -10,4 +10,87 @@ describe "NativeLib - Boolean Methods" do
     it_interprets %q((true && true).to_s),    [val("true")]
     it_interprets %q((false || false).to_s),  [val("false")]
   end
+
+  describe "#==" do
+    # Standard boolean truthtable
+    it_interprets %q(false  == false),  [val(true)]
+    it_interprets %q(false  == true),   [val(false)]
+    it_interprets %q(true   == false),  [val(false)]
+    it_interprets %q(true   == true),   [val(true)]
+
+    # Using #==, the argument must be a boolean with the same value. #== is
+    # _not_ a check for truthiness.
+    it_interprets %q(true   == nil),      [val(false)]
+    it_interprets %q(true   == 0),        [val(false)]
+    it_interprets %q(true   == 1),        [val(false)]
+    it_interprets %q(true   == 1.0),      [val(false)]
+    it_interprets %q(true   == "hello"),  [val(false)]
+    it_interprets %q(true   == :hi),      [val(false)]
+    it_interprets %q(true   == []),       [val(false)]
+    it_interprets %q(true   == [1, 2]),   [val(false)]
+    it_interprets %q(true   == {}),       [val(false)]
+    it_interprets %q(true   == {a: 1}),   [val(false)]
+
+    it_interprets %q(false  == nil),      [val(false)]
+    it_interprets %q(false  == 0),        [val(false)]
+    it_interprets %q(false  == 1),        [val(false)]
+    it_interprets %q(false  == 1.0),      [val(false)]
+    it_interprets %q(false  == "hello"),  [val(false)]
+    it_interprets %q(false  == :hi),      [val(false)]
+    it_interprets %q(false  == []),       [val(false)]
+    it_interprets %q(false  == [1, 2]),   [val(false)]
+    it_interprets %q(false  == {}),       [val(false)]
+    it_interprets %q(false  == {a: 1}),   [val(false)]
+
+    # Since #== always returns a boolean, it can be chained to do an equality
+    # comparison, followed by an assertion on true/false.
+    it_interprets %q(false  == false  == false),  [val(false)]
+    it_interprets %q(false  == true   == false),  [val(true)]
+    it_interprets %q(false  == false  == true),   [val(true)]
+    it_interprets %q(false  == true   == true),   [val(false)]
+    it_interprets %q(true   == false  == false),  [val(true)]
+    it_interprets %q(true   == true   == false),  [val(false)]
+    it_interprets %q(true   == false  == true),   [val(false)]
+    it_interprets %q(true   == true   == true),   [val(true)]
+  end
+
+  # Boolean#!= is the exact inverse of Boolean#==.
+  describe "#!=" do
+    it_interprets %q(false  != false),  [val(false)]
+    it_interprets %q(false  != true),   [val(true)]
+    it_interprets %q(true   != false),  [val(true)]
+    it_interprets %q(true   != true),   [val(false)]
+
+    it_interprets %q(true   != nil),      [val(true)]
+    it_interprets %q(true   != 0),        [val(true)]
+    it_interprets %q(true   != 1),        [val(true)]
+    it_interprets %q(true   != 1.0),      [val(true)]
+    it_interprets %q(true   != "hello"),  [val(true)]
+    it_interprets %q(true   != :hi),      [val(true)]
+    it_interprets %q(true   != []),       [val(true)]
+    it_interprets %q(true   != [1, 2]),   [val(true)]
+    it_interprets %q(true   != {}),       [val(true)]
+    it_interprets %q(true   != {a: 1}),   [val(true)]
+
+    it_interprets %q(false  != nil),      [val(true)]
+    it_interprets %q(false  != 0),        [val(true)]
+    it_interprets %q(false  != 1),        [val(true)]
+    it_interprets %q(false  != 1.0),      [val(true)]
+    it_interprets %q(false  != "hello"),  [val(true)]
+    it_interprets %q(false  != :hi),      [val(true)]
+    it_interprets %q(false  != []),       [val(true)]
+    it_interprets %q(false  != [1, 2]),   [val(true)]
+    it_interprets %q(false  != {}),       [val(true)]
+    it_interprets %q(false  != {a: 1}),   [val(true)]
+
+
+    it_interprets %q(false  != false  != false),  [val(false)]
+    it_interprets %q(false  != true   != false),  [val(true)]
+    it_interprets %q(false  != false  != true),   [val(true)]
+    it_interprets %q(false  != true   != true),   [val(false)]
+    it_interprets %q(true   != false  != false),  [val(true)]
+    it_interprets %q(true   != true   != false),  [val(false)]
+    it_interprets %q(true   != false  != true),   [val(false)]
+    it_interprets %q(true   != true   != true),   [val(true)]
+  end
 end
--- src/myst/interpreter/native_lib/boolean.cr
+++ src/myst/interpreter/native_lib/boolean.cr
@@ -2,4 +2,24 @@ module Myst
   TBoolean::METHODS["to_s"] = TNativeFunctor.new do |this, _args, _block, _itr|
     TString.new(this.as(TBoolean).value ? "true" : "false")
   end
+
+  TBoolean::METHODS["=="] = TNativeFunctor.new do |this, (arg), _block, _itr|
+    this = this.as(TBoolean)
+    case arg
+    when TBoolean
+      TBoolean.new(this.value == arg.value)
+    else
+      TBoolean.new(false)
+    end
+  end
+
+  TBoolean::METHODS["!="] = TNativeFunctor.new do |this, (arg), _block, _itr|
+    this = this.as(TBoolean)
+    case arg
+    when TBoolean
+      TBoolean.new(this.value != arg.value)
+    else
+      TBoolean.new(true)
+    end
+  end
 end
