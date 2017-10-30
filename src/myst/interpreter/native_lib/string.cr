0cf82b242bd65b7af87558cddcf2d15731072568
Jon Egeland <jonegeland@gmail.com>
2017-10-30 13:01:12 -0400

    [nativelib] Add `String#split`. `String#split` scans the string for instances of the delimiter (if given) or spaces (if no delimiter is given), separating the string into new substrings at each one. The result of the method is a List of the substrings. If the delimiter does not exist in the string, the result will be an array containing a copy of the original string.

--- src/myst/interpreter/native_lib/string.cr
+++ src/myst/interpreter/native_lib/string.cr
@@ -41,15 +41,28 @@ module Myst
       end
     end
 
+    NativeLib.method :string_split, TString do
+      delimiter =
+        case delim_arg = __args[0]?
+        when nil
+          " "
+        when TString
+          delim_arg.value
+        end
+
+      TList.new(this.value.split(delimiter).map{ |s| TString.new(s).as(Value) })
+    end
+
 
     def init_string(root_scope : Scope)
       string_type = TType.new("String", root_scope)
 
-      NativeLib.def_instance_method(string_type, :+,     :string_add)
-      NativeLib.def_instance_method(string_type, :*,     :string_multiply)
-      NativeLib.def_instance_method(string_type, :to_s,  :string_to_s)
-      NativeLib.def_instance_method(string_type, :==,    :string_eq)
-      NativeLib.def_instance_method(string_type, :!=,    :string_not_eq)
+      NativeLib.def_instance_method(string_type, :+,      :string_add)
+      NativeLib.def_instance_method(string_type, :*,      :string_multiply)
+      NativeLib.def_instance_method(string_type, :to_s,   :string_to_s)
+      NativeLib.def_instance_method(string_type, :==,     :string_eq)
+      NativeLib.def_instance_method(string_type, :!=,     :string_not_eq)
+      NativeLib.def_instance_method(string_type, :split,  :string_split)
 
       string_type
     end
