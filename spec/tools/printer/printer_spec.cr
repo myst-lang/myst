d7a0d0f93a219465173722503da3dd7b7588cfcb
Jon Egeland <jonegeland@gmail.com>
2017-11-01 11:15:18 -0400

    [tools:printer] Organize printer specs into `nodes`. Much like the interpreter specs and source, each node type will have it's own spec file under `spec/tools/printer/nodes/`, while more general printer tests will live outside the `nodes` folder.

--- spec/tools/printer/printer_spec.cr
+++ spec/tools/printer/printer_spec.cr
@@ -1,45 +1,21 @@
 require "./helper.cr"
 
-describe "Printer - Literals" do
-  # Nil
-  assert_print  %q(nil)
+describe "Printer" do
+  it "acts like a visitor" do
+    printer = Myst::Printer.new
+    printer.responds_to?(:visit).should be_true
+  end
 
-  # Booleans
-  assert_print  %q(true)
-  assert_print  %q(false)
+  it "has configurable IO" do
+    output1 = IO::Memory.new
+    output2 = IO::Memory.new
+    printer = Myst::Printer.new(output: output1)
+    printer.print(parse_program(%q(1)))
 
-  # Integers
-  assert_print  %q(1)
-  assert_print  %q(100)
-  assert_print  %q(100)
+    printer.output = output2
+    printer.print(parse_program(%q(2)))
 
-  # Floats
-  assert_print  %q(1.0)
-  assert_print  %q(1.345)
-  assert_print  %q(12345.67)
-
-  # Strings
-  assert_print  %q("h")
-  assert_print  %q("hello")
-  assert_print  %q("")
-
-
-  # Symbols
-  assert_print  %q(:hi)
-  assert_print  %q(:with_underscore)
-  assert_print  %q(:with_underscore)
-
-
-  # Lists
-  assert_print  %q([1, 2, 3])
-  assert_print  %q([nil, nil, nil])
-  assert_print  %q([nil, true, false])
-  assert_print  %q([true, false, 1])
-  assert_print  %q([:hello, "hi", 10.054])
-
-
-  # Maps
-  assert_print  %q({a: 1, b: 2})
-  assert_print  %q({a: "hello", b: :hi})
-  assert_print  %q({a: nil, b: false, c: 10.242, d: 3})
+    output1.to_s.should eq("1")
+    output2.to_s.should eq("2")
+  end
 end
