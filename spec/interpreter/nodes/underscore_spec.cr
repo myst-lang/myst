15c456b585f346adf8cb8ed942c16fdda1ab30ec
Jérôme Pasquier <rainbru42@gmail.com>
2017-11-09 16:50:04 +0100

    Add some unit tests comments

--- spec/interpreter/nodes/underscore_spec.cr
+++ spec/interpreter/nodes/underscore_spec.cr
@@ -6,7 +6,10 @@ describe "Interpreter - Underscore" do
   it_interprets %q(_ = 1; _),         [val(1)]
   it_interprets %q(_a = _b = {}; _b), [TMap.new]
 
+  # Underscore reference UTs
+  ## Continue parsing. It's only a warning, prg must compile and return value
   it_interprets %q(_a = 1;_b = 2;_c = 1 + 2; _c), [val(3)]
+  ## Simply test for number of warnings
   it_warns %q(_a = 1;_b = 2;_c = 5 + _b;), 1
   it_warns %q(_a = 1;_b = 2;_c = _a + _b;), 2
 end
