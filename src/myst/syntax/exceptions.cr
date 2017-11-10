54911ace77f10c6c705b346460fd845de9e8414c
Jon Egeland <jonegeland@gmail.com>
2017-11-10 17:48:52 -0500

    [parser] Add location information to ParseErrors. ParseErrors now show the exact location that the error occurred, which should help a lot with searching for errors in user programs.

--- src/myst/syntax/exceptions.cr
+++ src/myst/syntax/exceptions.cr
@@ -2,16 +2,35 @@ require "./location"
 
 module Myst
   class BaseException < Exception
-  end
-
-  class SyntaxError < BaseException
     property location : Location
 
     def initialize(@location, @message="")
-      @message = "Syntax error at #{@location}: #{@message}"
+      # We're showing a custom message. The implementation backtrace is just
+      # noise to end users.
+      @callstack = nil
+    end
+
+    # For some reason, simply setting `@callstack` to nil kept showing the
+    # backtrace. This ensures that the returned backtrace will always be empty.
+    def backtrace?
+      [] of String
+    end
+  end
+
+  class SyntaxError < BaseException
+    def initialize(@location, @message="")
+      super(@location, "Syntax error at #{@location}: #{@message}")
     end
   end
 
   class ParseError < BaseException
+    def initialize(@location, @message="")
+      super(@location,
+        <<-MESSAGE
+        ParseError at #{@location}
+          #{@message}
+        MESSAGE
+      )
+    end
   end
 end
