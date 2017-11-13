5e973c3786794f0be2addeab5d8cc26abeab5688
Jérôme Pasquier <rainbru42@gmail.com>
2017-11-13 05:38:21 +0100

    Add specs for negation on Myst::Var

--- spec/interpreter/nodes/unary_ops_spec.cr
+++ spec/interpreter/nodes/unary_ops_spec.cr
@@ -6,15 +6,17 @@ describe "Interpreter - OpUnary" do
   it "handle negation for integers" do
     it_interprets %q(a = 2 + -1; a), [val(1)]
     it_interprets %q(a = -2 + -1;), [val(-3)]
+    it_interprets %q(a = -2; -a), [val(2)]
   end
   
   it "handle negation for floats" do
     it_interprets %q(-2.23 + 1), [val(-1.23)]
     it_interprets %q(-2.23 + 1.1), [val(-1.13)]
+    it_interprets %q(a = 2.23; -a), [val(-2.23)]
   end
 
   it "handle negation for floats" do
     it_interprets %q(!true), [val(false)]
-    it_interprets %q(not true), [val(false)]
+    it_interprets %q(a=false; b = !a; b), [val(true)]
   end
 end
--- src/myst/interpreter/nodes/unary_ops.cr
+++ src/myst/interpreter/nodes/unary_ops.cr
@@ -16,5 +16,15 @@ module Myst
       result = Invocation.new(self, substract, null, [v] , nil).invoke
       stack.push(result)
     end
+
+    def visit(node : Not)
+      v = Value.from_literal(node.value)
+      if v.to_s == "true"
+        stack.push(TBoolean.new(false))
+      else
+        stack.push(TBoolean.new(true))
+      end
+    end
+
   end
 end
