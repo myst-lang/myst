7a52d9cf0b018a620c0c5ba05e293477e5348b53
Jérôme Pasquier <rainbru42@gmail.com>
2017-11-13 05:22:19 +0100

    Add Not node specs

--- spec/interpreter/nodes/unary_ops_spec.cr
+++ spec/interpreter/nodes/unary_ops_spec.cr
@@ -6,7 +6,15 @@ describe "Interpreter - OpUnary" do
   it "handle negation for integers" do
     it_interprets %q(a = 2 + -1; a), [val(1)]
     it_interprets %q(a = -2 + -1;), [val(-3)]
+  end
+  
+  it "handle negation for floats" do
     it_interprets %q(-2.23 + 1), [val(-1.23)]
     it_interprets %q(-2.23 + 1.1), [val(-1.13)]
+  end
+
+  it "handle negation for floats" do
+    it_interprets %q(!true), [val(false)]
+    it_interprets %q(not true), [val(false)]
   end
 end
