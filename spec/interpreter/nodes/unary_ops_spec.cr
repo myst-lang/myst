ae657748bfee3d57fee3f94091c43fa500289a1a
Jérôme Pasquier <rainbru42@gmail.com>
2017-11-13 03:40:02 +0100

    Handle negation both for integer and float

--- spec/interpreter/nodes/unary_ops_spec.cr
+++ spec/interpreter/nodes/unary_ops_spec.cr
@@ -3,7 +3,10 @@ require "../../support/nodes.cr"
 require "../../support/interpret.cr"
 
 describe "Interpreter - OpUnary" do
-  it "handle negation" do
+  it "handle negation for integers" do
     it_interprets %q(a = 2 + -1; a), [val(1)]
+    it_interprets %q(a = -2 + -1;), [val(-3)]
+    it_interprets %q(-2.23 + 1), [val(-1.23)]
+    it_interprets %q(-2.23 + 1.1), [val(-1.13)]
   end
 end
--- src/myst/interpreter/nodes/unary_ops.cr
+++ src/myst/interpreter/nodes/unary_ops.cr
@@ -2,9 +2,18 @@ module Myst
   class Interpreter
 
     def visit(node : Negation)
+      # Default case for Myst::IntegerLiteral
+      null = val(0)
+
+      # This block is used to avoid a cast error to TInteger to TFloat when
+      # calling :float_subtract
+      if  node.value.class == Myst::FloatLiteral
+        null = val(0.0)
+      end
+
       v = Value.from_literal(node.value)
-      func = self.__scopeof(v)["-"].as(TFunctor)
-      result = Invocation.new(self, func, val(0), [v] , nil).invoke
+      substract = self.__scopeof(v)["-"].as(TFunctor)
+      result = Invocation.new(self, substract, null, [v] , nil).invoke
       stack.push(result)
     end
   end
