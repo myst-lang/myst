e5dac12c0efaa8e93d6a8bf198f9cfad23a6c53b
Jens <jens0512@osloskolen.no>
2018-01-20 21:48:14 +0100

    Added top-level #sleep

--- src/myst/interpreter/native_lib/top_level.cr
+++ src/myst/interpreter/native_lib/top_level.cr
@@ -11,9 +11,21 @@ module Myst
       exit(real_status.to_i32)
     end
 
+    NativeLib.method :mt_sleep, Value, time : TInteger | TFloat? = nil do
+      
+      if time.is_a? TInteger || time.is_a? TFloat
+        sleep(time.value)
+      else
+        sleep
+      end
+
+      TNil.new
+    end
 
     def init_top_level(kernel : TModule)
       NativeLib.def_method(kernel, :exit, :mt_exit)
+      NativeLib.def_method(kernel, :sleep, :mt_sleep)
+
       kernel
     end
   end
