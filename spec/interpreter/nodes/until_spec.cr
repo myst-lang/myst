9e1c0d6058fa70435cdff7b56172707780330687
Jon <jonegeland@gmail.com>
2018-02-14 15:43:39 -0500

    [spec,interpreter] Add proper specs for `while` and `until`, catch `break` exceptions to stop looping. The specs for `while` and `until` weren't actually testing any of their behavior. The new specs should not be considered complete, but at least cover the basic use cases, including `break` from within a loop.
    Loop expressions also now properly catch `BreakException`s to stop iteration and optionally return a value. Because `BreakException` pushes a value onto the stack, no additional work is needed to handle this case.

--- spec/interpreter/nodes/until_spec.cr
+++ spec/interpreter/nodes/until_spec.cr
@@ -3,21 +3,55 @@ require "../../support/nodes.cr"
 require "../../support/interpret.cr"
 
 describe "Interpreter - Until" do
+  # `until` repeatedly executes its body as long as its condition is falsey.
   it_interprets %q(
     a = 1
-    until a == 1
-      a = 2
-      "ran"
+    until a >= 3
+      a += 1
     end
-  ),                  [val(nil)]
+
+    a
+  ),                  [val(3)]
+
+  # If the condition is immediately truthy, the body is never run.
+  it_interprets %q(
+    a = 1
+    until a >= 1
+      a += 1
+    end
+
+    a
+  ),                  [val(1)]
+
+  # By default, `until` returns `nil`.
   it_interprets %q(
     a = 1
     until a != 1
-      "ran"
+      a = 2
     end
   ),                  [val(nil)]
+
+  # Looping can be stopped early by using `break`.
   it_interprets %q(
-    a = 1
-    until a != 1; end
-  ),                  [val(nil)]
+    x = 0
+    until x >= 5
+      when x == 2
+        break
+      end
+      x += 1
+    end
+
+    x
+  ),                  [val(2)]
+
+  # Using `break` with a value sets the return value of the `until`.
+  it_interprets %q(
+    x = 0
+    until x >= 5
+      when x == 2
+        break x
+      end
+      x += 1
+    end
+  ),                  [val(2)]
 end
--- spec/interpreter/nodes/while_spec.cr
+++ spec/interpreter/nodes/while_spec.cr
@@ -3,21 +3,55 @@ require "../../support/nodes.cr"
 require "../../support/interpret.cr"
 
 describe "Interpreter - While" do
+  # `while` repeatedly executes its body as long as its condition is truthy.
+  it_interprets %q(
+    a = 1
+    while a < 3
+      a += 1
+    end
+
+    a
+  ),                  [val(3)]
+
+  # If the condition is immediately falsey, the body is never run.
+  it_interprets %q(
+    a = 1
+    while a < 1
+      a += 1
+    end
+
+    a
+  ),                  [val(1)]
+
+  # By default, `while` returns `nil`.
   it_interprets %q(
     a = 1
     while a == 1
       a = 2
-      "ran"
     end
   ),                  [val(nil)]
+
+  # Looping can be stopped early by using `break`.
   it_interprets %q(
-    a = 1
-    while a != 1
-      "ran"
+    x = 0
+    while x < 5
+      when x == 2
+        break
+      end
+      x += 1
     end
-  ),                  [val(nil)]
+
+    x
+  ),                  [val(2)]
+
+  # Using `break` with a value sets the return value of the `while`.
   it_interprets %q(
-    a = 1
-    while a != 1; end
-  ),                  [val(nil)]
+    x = 0
+    while x < 5
+      when x == 2
+        break x
+      end
+      x += 1
+    end
+  ),                  [val(2)]
 end
